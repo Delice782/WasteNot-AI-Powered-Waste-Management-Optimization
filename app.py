134bceca3e22d123d06805a199ebaf40a579efdc
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-09-27 16:49:36 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
                                                                                                    
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify 
 import pickle
 import numpy as np
 
